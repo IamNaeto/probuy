4589290c51bca176024df7c6e11f7474ac80b231
IamNaeto <egesionucharlesobimnaeto@gmail.com>
2023-07-20 13:21:37 +0100

    implemented mobile responsiveness

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-// On scroll chanage nav bar bg color
-$(document).ready(function() {
-    // Function to change the background color of the navbar on scroll
-    function changeNavbarBackground() {
-      var navbar = document.getElementById("navbar");
-      var offset = 100; //Determines when the background color changes
-
-      if (window.pageYOffset > offset) {
-        navbar.classList.add("scrolled");
-      } else {
-        navbar.classList.remove("scrolled");
-      }
-    }
-
-    // Attaching the scroll event listener to the window
-    window.addEventListener("scroll", changeNavbarBackground);
-  });
+// Nav Bar Scrolling Change
+window.addEventListener("scroll", function () {
+  let navbar = document.querySelector(".nav");
+  navbar.classList.toggle("scrolled", window.scrollY > 0);
+})
 
 
 //  Owl Carousel
@@ -25,9 +13,9 @@ $(document).ready(function() {
       responsiveClass: true,
       responsive: {
         0: {
-          items: 1.5,
+          items: 1.2,
         },
-        300: {
+        500: {
           items: 2.5,
         },
         600: {
@@ -51,3 +39,13 @@ $(document).ready(function() {
       owl.trigger("next.owl.carousel");
     });
   });
+
+
+// Hamburger script start
+const hamMenu = document.querySelector(".hamburger-menu");
+const offScreenMenu = document.querySelector(".nav-links");
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
+// Hamburger script end
